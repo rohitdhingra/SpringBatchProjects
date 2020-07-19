d8a8d255b41061e7a5987af78d27376a47a7efc5
Rohit Dhingra <er.rohit.dhingra@gmail.com>
2020-07-19 19:49:48 +0530

    Multithreaded job

--- ChunkBasedBatchProject/sql/DDLScript.sql
+++ ChunkBasedBatchProject/sql/DDLScript.sql
@@ -11,3 +11,16 @@ CREATE TABLE `shipped_order_output` (
   `item_name` varchar(15) DEFAULT NULL,
   `ship_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `TRACKED_ORDER` (
+  `order_id` int(11) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `cost` varchar(50) DEFAULT NULL,
+  `item_id` varchar(40) DEFAULT NULL,
+  `item_name` varchar(15) DEFAULT NULL,
+  `ship_date` date DEFAULT NULL,
+  `tracking_number` varchar(1000) default null,
+  `free_shipping` boolean default null  
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
