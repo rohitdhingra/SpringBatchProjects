1e1db3afd11ff4e1ce1fad5fc49efc26be0cb6dc
Rohit Dhingra <er.rohit.dhingra@gmail.com>
2020-07-19 20:51:01 +0530

    Quartz Scheduler Setup

--- ScheduleBatchJobs/src/main/java/com/example/ScheduleBatchJobsApplication.java
+++ ScheduleBatchJobs/src/main/java/com/example/ScheduleBatchJobsApplication.java
@@ -1,9 +1,16 @@
 package com.example;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.Step;
@@ -11,7 +18,9 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
@@ -23,12 +32,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.quartz.QuartzJobBean;
 
 @SpringBootApplication
 @EnableBatchProcessing
 @EnableScheduling
-public class ScheduleBatchJobsApplication {
+public class ScheduleBatchJobsApplication extends QuartzJobBean{
 
 	@Autowired
 	private JobBuilderFactory jobBuilderFactory;
@@ -38,14 +47,52 @@ public class ScheduleBatchJobsApplication {
 	
 	@Autowired
 	private JobLauncher jobLauncher;
+
+	@Autowired
+	public JobExplorer jobExplorer;
 	
-	@Scheduled(cron = "0/30 * * * * *")
-	public void runJob() throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException
+	@Bean
+	public Trigger trigger()
 	{
-		JobParametersBuilder paramBuilder = new JobParametersBuilder();
-		paramBuilder.addDate("runTime", new Date());
-		this.jobLauncher.run(job(), paramBuilder.toJobParameters());
+		SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
+				.withIntervalInSeconds(30)
+				.repeatForever();
+		
+		return TriggerBuilder.newTrigger()
+				.forJob(jobDetail())
+				.withSchedule(scheduleBuilder)
+				.build();
 	}
+	
+	@Bean
+	public JobDetail jobDetail()
+	{
+		return JobBuilder.newJob(ScheduleBatchJobsApplication.class)
+				.storeDurably()
+				.build();
+	}
+	
+	@Override
+	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+		JobParameters parameters  = new JobParametersBuilder(jobExplorer)
+				.getNextJobParameters(job()).toJobParameters();
+		try {
+			this.jobLauncher.run(job(), parameters);
+		} catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException
+				| JobParametersInvalidException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	
+//	@Scheduled(cron = "0/30 * * * * *")
+//	public void runJob() throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException
+//	{
+//		JobParametersBuilder paramBuilder = new JobParametersBuilder();
+//		paramBuilder.addDate("runTime", new Date());
+//		this.jobLauncher.run(job(), paramBuilder.toJobParameters());
+//	}
 	
 	@Bean
 	public Step step() {
@@ -65,7 +112,7 @@ public class ScheduleBatchJobsApplication {
 	@Bean
 	public Job job()
 	{
-		return this.jobBuilderFactory.get("job").start(step()).build();
+		return this.jobBuilderFactory.get("job").incrementer(new RunIdIncrementer()).start(step()).build();
 	}
 	
 	
@@ -74,4 +121,5 @@ public class ScheduleBatchJobsApplication {
 		SpringApplication.run(ScheduleBatchJobsApplication.class, args);
 	}
 
+	
 }
