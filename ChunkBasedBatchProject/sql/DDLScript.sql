6f3a487ef87b54d4bcae4f8cbff83929f481a358
Rohit Dhingra <er.rohit.dhingra@gmail.com>
2020-07-19 15:24:49 +0530

    Updated DDL script for output table

--- ChunkBasedBatchProject/sql/DDLScript.sql
+++ ChunkBasedBatchProject/sql/DDLScript.sql
@@ -1,2 +1,13 @@
 create table shipped_order(order_id int,first_name varchar(50),last_name varchar(50),email varchar(50),cost varchar(50),item_id varchar(40),
 item_name varchar(15),ship_date date); 
+
+CREATE TABLE `shipped_order_output` (
+  `order_id` int(11) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `cost` varchar(50) DEFAULT NULL,
+  `item_id` varchar(40) DEFAULT NULL,
+  `item_name` varchar(15) DEFAULT NULL,
+  `ship_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
